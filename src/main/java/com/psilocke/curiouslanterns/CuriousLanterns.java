e5a6b0baaa9d5fbec7400d8183bade39a57417e5
Psilocke <rhysculley10@gmail.com>
2022-05-06 17:09:37 -0500

    build curio slot implementation

---
+++ src/main/java/com/psilocke/curiouslanterns/CuriousLanterns.java
@@ -0,0 +1,69 @@
+package com.psilocke.curiouslanterns;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotTypeMessage;
+
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("curiouslanterns")
+public class CuriousLanterns {
+	public static final String MOD_ID = "curiouslanterns";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public CuriousLanterns()
+    {
+
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::TexStitchEventSub);
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void TexStitchEventSub(TextureStitchEvent.Pre event) {
+      event.addSprite(new ResourceLocation(MOD_ID, "gui/empty_hip_slot"));
+    }
+    
+    private void setup(final FMLCommonSetupEvent event) {
+    	
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+        InterModComms.sendTo(CuriosApi.MODID, SlotTypeMessage.REGISTER_TYPE, () -> new SlotTypeMessage.Builder("hip").icon(new ResourceLocation(MOD_ID, "gui/inventory")).build());
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+        LOGGER.info("Got IMC {}", event.getIMCStream().
+                map(m->m.messageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
