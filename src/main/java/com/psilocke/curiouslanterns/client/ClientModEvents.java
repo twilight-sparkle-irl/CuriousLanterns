22ddf648990e27c02ea0e5500e9ae8e923f490d2
Psilocke <rhysculley10@gmail.com>
2022-09-02 04:09:34 -0500

    Implemented automated model creation

--- src/main/java/com/psilocke/curiouslanterns/client/ClientModEvents.java
+++ src/main/java/com/psilocke/curiouslanterns/client/ClientModEvents.java
@@ -4,7 +4,6 @@ import com.psilocke.curiouslanterns.CuriousLanterns;
 import com.psilocke.curiouslanterns.curios.LanternRenderer;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Items;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ForgeModelBakery;
@@ -18,33 +17,42 @@ import top.theillusivec4.curios.api.client.CuriosRendererRegistry;
 @Mod.EventBusSubscriber(modid = CuriousLanterns.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientModEvents {
 	
+	public final static String[] lanterns = {
+			"minecraft:lantern",
+			"minecraft:soul_lantern",
+			"supplementaries:copper_lantern",
+			"supplementaries:crimson_lantern",
+			"supplementaries:silver_lantern",
+			"supplementaries:brass_lantern",
+			"supplementaries:lead_lantern",
+			"cyclic:gold_lantern",
+			"cyclic:gold_soul_lantern",
+			"cyclic:copper_lantern",
+			"cyclic:copper_soul_lantern",
+			"cyclic:netherite_lantern"
+	};
+	
 	@SubscribeEvent
 	public static void onClientSetupEvent(FMLClientSetupEvent e) {
-		CuriosRendererRegistry.register(Items.LANTERN, LanternRenderer::new);
-		CuriosRendererRegistry.register(Items.SOUL_LANTERN, LanternRenderer::new);
-		
-		if(ModList.get().isLoaded("supplementaries")) {
-			CuriosRendererRegistry.register(ForgeRegistries.ITEMS.getValue(new ResourceLocation("supplementaries:copper_lantern")), LanternRenderer::new);
-			CuriosRendererRegistry.register(ForgeRegistries.ITEMS.getValue(new ResourceLocation("supplementaries:crimson_lantern")), LanternRenderer::new);
-			CuriosRendererRegistry.register(ForgeRegistries.ITEMS.getValue(new ResourceLocation("supplementaries:silver_lantern")), LanternRenderer::new);
-			CuriosRendererRegistry.register(ForgeRegistries.ITEMS.getValue(new ResourceLocation("supplementaries:brass_lantern")), LanternRenderer::new);
-			CuriosRendererRegistry.register(ForgeRegistries.ITEMS.getValue(new ResourceLocation("supplementaries:lead_lantern")), LanternRenderer::new);
-			CuriosRendererRegistry.register(ForgeRegistries.ITEMS.getValue(new ResourceLocation("supplementaries:gold_lantern")), LanternRenderer::new);
+		for(String var : lanterns) {
+			String namespace = var.substring(0, var.indexOf(':'));
+			String item = var.substring(var.indexOf(':')+1);
+			
+			if(ModList.get().isLoaded(namespace)) {
+				CuriosRendererRegistry.register(ForgeRegistries.ITEMS.getValue(new ResourceLocation(namespace, item)), LanternRenderer::new);
+			}
 		}
 	}
 	
 	@SubscribeEvent
 	public static void onModelregister(ModelRegistryEvent event) {
-		ForgeModelBakery.addSpecialModel(new ResourceLocation("minecraft", "block/lantern"));
-		ForgeModelBakery.addSpecialModel(new ResourceLocation("minecraft", "block/soul_lantern"));
-		
-		if(ModList.get().isLoaded("supplementaries")) {
-			ForgeModelBakery.addSpecialModel(new ResourceLocation("supplementaries", "block/copper_lantern"));
-			ForgeModelBakery.addSpecialModel(new ResourceLocation("supplementaries", "block/crimson_lantern"));
-			ForgeModelBakery.addSpecialModel(new ResourceLocation("supplementaries", "block/silver_lantern"));
-			ForgeModelBakery.addSpecialModel(new ResourceLocation("supplementaries", "block/brass_lantern"));
-			ForgeModelBakery.addSpecialModel(new ResourceLocation("supplementaries", "block/lead_lantern"));
-			ForgeModelBakery.addSpecialModel(new ResourceLocation("supplementaries", "block/gold_lantern"));
+		for(String var : lanterns) {
+			String namespace = var.substring(0, var.indexOf(':'));
+			String item = var.substring(var.indexOf(':')+1);
+			
+			if(ModList.get().isLoaded(namespace)) {
+				ForgeModelBakery.addSpecialModel(new ResourceLocation(namespace, "block/" + item));
+			}
 		}
 	}
 }
